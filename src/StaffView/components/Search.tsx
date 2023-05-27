eee2ae80daaefcb593ba6a80a31fe1d1d09cee10
Thương Lê Đăng <ledangthuong2003@gmail.com>
2023-05-27 21:33:48 +0700

    Xin đổi lại phần search để làm dễ hơn

--- src/StaffView/components/Search.tsx
+++ src/StaffView/components/Search.tsx
@@ -1,39 +1,61 @@
-import React from "react";
-import { StyleSheet, Text, TextInput, View, Image } from "react-native";
+import React, {useState} from "react";
+import { StyleSheet, Text, TextInput, View, Image} from "react-native";
 import { SearchIcon } from "../../CustomerView/assets/icons/index.js";
+import {colors, sizes, spacing, shadow} from "../constants/theme.js"
 import CUSTOM_COLOR from "../constants/colors";
 
-const Search = (props: any) =>{
-
-  return(
-    <View style = {{
-      flexDirection: 'row', 
-      borderWidth: 1, 
-      alignItems: 'center',
-      padding: 10,
-      borderRadius: 20,
-      height: 40,
-      width: 370,
-      backgroundColor: CUSTOM_COLOR.White,
-      margin: 10,
-      ...props.style
-        }}>
-      <Image
-        style = {{
-          marginRight: 5,
-          width: 20,
-          height: 20
-        }}
-        source = {SearchIcon}
-       
-      />
-      <TextInput style = {{width: '70%', height: 30, borderWidth: 0, padding:5}}
-        placeholder = {props.placeholder}
-      />
-      
+const Search = () => {
+  const [search, setSearch] = useState('');
+  return (
+    <View style={styles.container}>
+      <View style={styles.inner}>
+        <View style={styles.search} pointerEvents="none">
+          <SearchIcon icon="Search" />
+        </View>
+        <TextInput
+          style={styles.field}
+          placeholder="Search"
+          value={search}
+          onChangeText={setSearch}
+        />
+        <View style={styles.filter}>
+          <SearchIcon icon="Filter" onPress={() => {}} />
+        </View>
+      </View>
     </View>
-  )
-   
+  );
 };
 
-export default Search
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: spacing.l,
+    paddingTop: spacing.l,
+    paddingBottom: spacing.l / 1.5,
+  },
+  inner: {
+    flexDirection: 'row',
+  },
+  search: {
+    position: 'absolute',
+    top: 10,
+    left: 10,
+    zIndex: 1,
+  },
+  field: {
+    backgroundColor: colors.white,
+    paddingLeft: spacing.xl + spacing.s,
+    paddingRight: spacing.m,
+    paddingVertical: spacing.m,
+    borderRadius: sizes.radius,
+    height: 54,
+    flex: 1, ...shadow.light,
+  },
+  filter: {
+    position: 'absolute',
+    top: 10,
+    right: 10,
+    zIndex: 1,
+  },
+});
+
+export default Search;
